eef51fb8f059fbb76eaea0111ef4cf298e7f900b
stephanedescarpentries <stephane.descarpentries@hotmail.fr>
2024-12-10 23:47:53 +0100

    merge Willex with main

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'hetic/parisecran',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4ce96aa6f6920108fa97f710222804c6eb9ef424',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'hetic/parisecran' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4ce96aa6f6920108fa97f710222804c6eb9ef424',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
