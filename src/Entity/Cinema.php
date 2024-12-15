a34f92c88da6f02067b1ee5149b7e1f86200b6c1
stephanedescarpentries <stephane.descarpentries@hotmail.fr>
2024-12-15 16:05:09 +0100

    update cinemas methode

--- src/Entity/Cinema.php
+++ src/Entity/Cinema.php
@@ -21,28 +21,51 @@ class Cinema
 
     public function getDirectorsByCinema($orderBy): array
     {
-        $query = "SELECT 
-                    ci.id AS cinema_id,
-                    ci.name AS cinemaName,
-                    ci.borough AS borough,
-                    ci.address AS address,
-                    ci.presentation AS presentation,
-                    ci.address AS Address,
-                    ST_X(ci.geolocation) AS longitude,
-                    ST_Y(ci.geolocation) AS latitude,
-                    ci.phone AS phone,
-                    ci.email AS email,
-            GROUP_CONCAT(DISTINCT CONCAT(c.firstName, ' ', c.lastName) ORDER BY c.lastName ASC SEPARATOR ', ') AS directors,
-            AVG(com.notation) AS AverageRating
-            FROM cinema ci
-            JOIN room AS ro ON ci.id = ro.cinema_id
-            JOIN seance AS s ON ro.id = s.room_id
-            JOIN film AS f ON s.film_id = f.id
-            JOIN role AS r ON f.id = r.film_id
-            JOIN casting AS c ON r.casting_id = c.id
-            LEFT JOIN comment AS com ON f.id = com.film_id
-            WHERE r.role = 'realisateur'
-            GROUP BY ci.id
+        $query = "WITH Directors AS (
+                SELECT
+                    f.id AS film_id,
+                    GROUP_CONCAT(DISTINCT CONCAT(c.firstName, ' ', c.lastName) ORDER BY c.lastName ASC SEPARATOR ', ') AS directors
+                FROM
+                    film f
+                JOIN role r ON f.id = r.film_id
+                JOIN casting c ON r.casting_id = c.id
+                WHERE
+                    r.role = 'realisateur'
+                GROUP BY
+                    f.id
+            ),
+            FilmRatings AS (
+                SELECT
+                    f.id AS film_id,
+                    AVG(com.notation) AS AverageRating
+                FROM
+                    film f
+                LEFT JOIN comment com ON f.id = com.film_id
+                GROUP BY
+                    f.id
+            )
+            SELECT
+                ci.id AS cinema_id,
+                ci.name AS cinemaName,
+                ci.borough AS borough,
+                ci.address AS address,
+                ci.presentation AS presentation,
+                ci.address AS Address,
+                ST_X(ci.geolocation) AS longitude,
+                ST_Y(ci.geolocation) AS latitude,
+                ci.phone AS phone,
+                ci.email AS email,
+                d.directors,
+                fr.AverageRating
+            FROM
+                cinema ci
+            JOIN room ro ON ci.id = ro.cinema_id
+            JOIN seance s ON ro.id = s.room_id
+            JOIN film f ON s.film_id = f.id
+            LEFT JOIN Directors d ON f.id = d.film_id
+            LEFT JOIN FilmRatings fr ON f.id = fr.film_id
+            GROUP BY
+                ci.id
             ORDER BY $orderBy;";
     
         $stmt = $this->connector->query($query);
